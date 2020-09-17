e3ec04c968c31b8e79e73c8b706f11c9d4c911c5
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2020-09-18 05:59:04 +0700

    init

---
+++ my-app/src/components/intro/intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div>
+        <p>Hi, I'm MINHBB</p>
+        <p>I'm a full stack developer</p>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ my-app/src/services/data.js
@@ -0,0 +1,51 @@
+export const skillsData = [
+  { name: 'html', category: 'frontend' },
+  { name: 'css', category: 'frontend' },
+  { name: 'sass', category: 'frontend' },
+  { name: 'javascript', category: 'language' },
+  { name: 'typescript', category: 'other' },
+  { name: 'C#', category: 'language' },
+  { name: 'react', category: 'frontend' },
+  { name: 'angular', category: 'frontend' },
+  { name: 'ionic', category: 'frontend' },
+  { name: 'node.js', category: 'backend' },
+  { name: 'nestjs', category: 'backend' },
+  { name: 'express.js', category: 'backend' },
+  { name: 'sql', category: 'backend' },
+  { name: 'ASP.NET', category: 'backend' },
+  { name: 'git', category: 'other' },
+];
+
+skillsData.forEach((e, i) => (e.id = i));
+
+export const projectsData = [
+  {
+    name: 'Fifteen',
+    description: `Web app built with the MERN stack. Add /admin to the URL and login as admin 
+                  to modify the data by sending HTTP requests to the server. Credentials:<br>
+                  <b>username:</b> admin, <b>password:</b> my_password`,
+    imgName: 'fifteen.png',
+    siteUrl: 'https://fifteen-app.herokuapp.com/',
+    githubUrl: 'https://github.com/Gilbert1391/fifteen-webapp',
+  },
+  {
+    name: 'Dashboard box',
+    description: `Single Page Application (SPA) implemented with React, I used the 
+                  movie database API to fetch dynamic data to the app. Check repo on 
+                  GitHub for more information.`,
+    imgName: 'dashboard-box.png',
+    siteUrl: 'https://dashboardbox.netlify.com/#/',
+    githubUrl: 'https://github.com/Gilbert1391/dashboardbox',
+  },
+  {
+    name: 'Bryant hill',
+    description: `I built this website thinking about real estate, 
+                  it has galleries and individual listing pages. 
+                  I integrated some vanilla JavaScript functionalities.`,
+    imgName: 'bryant-hill.png',
+    siteUrl: 'https://gilbert1391.github.io/Bryant-Hill/',
+    githubUrl: 'https://github.com/Gilbert1391/Bryant-Hill',
+  },
+];
+
+projectsData.forEach((e, i) => (e.id = i));
